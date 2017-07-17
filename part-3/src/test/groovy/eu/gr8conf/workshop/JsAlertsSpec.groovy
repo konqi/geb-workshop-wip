b0c5b5f3e0f3d475aef2dc6c1ac90c862c4e5c38
Johann Wagner <johann.wagner@codecentric.de>
2017-07-17 14:35:37 +0200

    started with part 3

--- part-3/src/test/groovy/eu/gr8conf/workshop/JsAlertsSpec.groovy
+++ part-3/src/test/groovy/eu/gr8conf/workshop/JsAlertsSpec.groovy
@@ -9,20 +9,21 @@ class JsAlertsSpec extends GebReportingSpec {
 
     void "verify javascript alert can be handled - displaying 'You successfuly clicked an alert'"() {
         setup:
-        go "http://the-internet.herokuapp.com/javascript_alerts"
-
-        expect: 'Needs implementation'
-        false
+            go "http://the-internet.herokuapp.com/javascript_alerts"
+        when:
+            withAlert { $("ul li button", 0).click() }
+        then:
+            $('p#result').text() == 'You successfuly clicked an alert'
     }
 
-    @Unroll
-    void "verify javascript confirm can be handled - accepting and rejecting displaying 'You Clicked'"() {
-        setup:
-        go "http://the-internet.herokuapp.com/javascript_alerts"
-
-        expect: 'Needs implementation'
-        false
-    }
+//    @Unroll
+//    void "verify javascript confirm can be handled - accepting and rejecting displaying 'You Clicked'"() {
+//        setup:
+//        go "http://the-internet.herokuapp.com/javascript_alerts"
+//
+//        expect: 'Needs implementation'
+//        false
+//    }
 
 
 
